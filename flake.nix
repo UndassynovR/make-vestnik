32b32c7c524fd187c8df957c991976b98b280c00
Ramil <undassynovramil@gmail.com>
2025-11-25 11:54:49 +0500

    3rd time

--- flake.nix
+++ flake.nix
@@ -6,23 +6,30 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-      in {
+      in
+      {
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "make-vestnik";
           version = "1.0.0";
           src = ./.;
-
           cargoLock.lockFile = ./Cargo.lock;
 
           nativeBuildInputs = [
             pkgs.pkg-config
+            pkgs.makeWrapper
           ];
 
-          # runtime dependencies
+          # runtime deps
           propagatedBuildInputs = with pkgs; [
             pandoc
             texlive.combined.scheme-full
@@ -30,15 +37,17 @@
             ghostscript
           ];
 
-          # install templates in $out/share/make-vestnik/templates
           postInstall = ''
             mkdir -p $out/share/make-vestnik/templates
             cp -r $src/template/* $out/share/make-vestnik/templates/
+
+            # wrap the binary so 'gs' is on PATH for ImageMagick
+            wrapProgram $out/bin/make-vestnik \
+              --prefix PATH : ${pkgs.ghostscript}/bin
           '';
 
           meta = with pkgs.lib; {
-            description =
-              "LaTeX document project manager — converts DOCX, updates projects, and compiles XeLaTeX";
+            description = "LaTeX document manager";
             license = licenses.mit;
           };
         };
@@ -66,6 +75,6 @@
             ${self.packages.${system}.default}/bin/make-vestnik
           '';
         };
-      });
+      }
+    );
 }
-
